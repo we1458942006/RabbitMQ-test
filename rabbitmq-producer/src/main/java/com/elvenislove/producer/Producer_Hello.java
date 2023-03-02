f1b45b0b210061d4b4b3d1f30abe515e4e3ada52
we1458942006 <71440398+we1458942006@users.noreply.github.com>
2023-03-02 22:21:15 +0800

    初级学习方案

---
+++ rabbitmq-producer/src/main/java/com/elvenislove/producer/Producer_Hello.java
@@ -0,0 +1,69 @@
+package com.elvenislove.producer;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+
+
+/**
+ * @author elven
+ * @version 1.0.0
+ * @date 2020/12/25 15:59
+ * 发送消息
+ */
+public class Producer_Hello {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        // 建立连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+
+        // 设置参数IP
+        factory.setHost("127.0.0.1");
+        // 默认端口 ：5672
+        factory.setPort(1099);
+        // 虚拟机默认值：/
+        factory.setVirtualHost("/");
+        // 用户名 默认值：guest
+        factory.setUsername("guest");
+        // 密码 默认值：guest
+        factory.setPassword("guest");
+        // 创建链接
+        Connection conn = factory.newConnection();
+        // 创建channel
+        Channel channel = conn.createChannel();
+        /**
+         * channel.queueDeclare - 队列声明
+         *  - 参数：
+         *    - queue:队列名称
+         *    - declare :是否持久化
+         *    - exclusive:是否独占，只能有一个消费者监听这个队列
+*             - autoDelete:是否自动删除
+         *    - 最大容量
+         *    - 最大存量
+         *    - 如果没有队列叫这个名字的队列，就会创建，反之不会
+         */
+        channel.queueDeclare("hello", true, false, false, null);
+
+        /**
+         * 发送信息
+         * exchange - 交换机名称
+         * routingKey - 路由键（名称）
+         * properties - 消息属性
+         * body - 消息体
+         * message - 消息体
+         */
+        channel.basicPublish("", "hello", null, "Hello World".getBytes());
+
+
+        // 释放资源
+        channel.close();
+        conn.close();
+
+
+    }
+}
